730793a7e4c03694b7389b8d0210a38bf0fcd076
Tom McLaughlin <tom@serverlessops.io>
2019-02-10 18:14:40 -0500

    Update routes

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,23 @@ from flask import jsonify
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def main():
-    '''Entry point'''
+@app.route('/hello', methods=['GET'])
+def hello():
+    '''Hello World'''
     message = {'message': 'Hello, World!'}
     resp = jsonify(message)
     resp.status_code = 200
 
     return resp
+
+
+@app.route('/health', methods=['GET'])
+def health():
+    '''Health endpoint'''
+    message = {'health': 'OK'}
+    resp = jsonify(message)
+    resp.status_code = 200
+
+    return resp
+
 
